3e4dbb049377aaee973067b6379322151cc388c2
Ravi8972 <120507471+Ravi8972@users.noreply.github.com>
2023-06-19 23:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+// Get the line element
+var line = document.getElementById('line');
+
+// Set the initial rotation angle and center point of the line
+var rotationAngle = 0;
+var centerX = line.offsetWidth / 2;
+var centerY = line.offsetHeight / 2;
+
+// Function to rotate the line
+function rotateLine() {
+  rotationAngle += 1; // Increase the rotation angle
+
+  // Apply the rotation using CSS transform property
+  line.style.transform = 'rotate(' + rotationAngle + 'deg)';
+
+  // Request the next animation frame
+  requestAnimationFrame(rotateLine);
+}
+
+// Start rotating the line
+rotateLine();
+
